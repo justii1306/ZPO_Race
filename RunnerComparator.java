db0e8b560de74045746fca02ddf5d5786468714e
justii1306 <35031460+justii1306@users.noreply.github.com>
2019-01-22 15:57:49 +0100

    Update RunnerComparator.java

--- RunnerComparator.java
+++ RunnerComparator.java
@@ -3,7 +3,7 @@ import java.util.Comparator;
 public class RunnerComparator implements Comparator<Runner> { //Komparator potrzebny do listy automatycznie sortowanej, sortuje według tego
     @Override
     public int compare(Runner o1, Runner o2) {
-        int result = Integer.compare(o1.getTime(), o2.getTime()); //Poruwnaj czasy wyścigu zawodników
+        int result = Integer.compare(o1.getTime(), o2.getTime()); //Porównaj czasy wyścigu zawodników
         if (result == 0) //Jeśli są równe
             result = Integer.compare(o1.getStartTime(), o2.getStartTime()); //To pierwszy w kolejce jest ten co wcześniej zaczął (ktoś musi być pierwszy, a nie mogą zacząć w tym samym momencie)
         return result;
